8e98a7e168108df765d8c0bc9b7754f0b164173e
jeffzhao520 <zhao.no.1@163.com>
2018-08-20 23:32:43 +0800

    add git ignore

--- src/main/java/com/jeff/bella/aggregator/controller/BellaController.java
+++ src/main/java/com/jeff/bella/aggregator/controller/BellaController.java
@@ -2,14 +2,15 @@ package com.jeff.bella.aggregator.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-    /**
-     * Created by dawang on 16/7/20.
-     */
-    @RestController
-    public class BellaController {
-        @RequestMapping("/hello")
-        public String index() {
-            return "Hello World! This is Jeff.";
-        }
+
+/**
+ * Created by Jeff.
+ */
+@RestController
+public class BellaController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World! This is Jeff.";
     }
+}
 
